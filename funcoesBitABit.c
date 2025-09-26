8334f559fc63b35c1c5648278076cfcd1fe9d9b9
joaoclemos <joaoclemos22@gmail.com>
2025-09-26 12:16:44 -0300

    Q4

---
+++ funcoesBitABit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int codigoDeArea(unsigned int number) {
+    return number/1000000000;
+}
+
+int parEmImpar(unsigned int number) {
+    return number | 1;
+}
+
+
+int par(unsigned int number) {
+    return number & 1;
+}
